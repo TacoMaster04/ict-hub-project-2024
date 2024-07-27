56f5bb21555603e9804e6dc32091c06e3bcdcdc2
Mazen Laptop <mazen.a.04@gmail.com>
2024-07-27 15:18:32 +0300

    chore: add final section

---
+++ assets/amr_code/code.js
@@ -0,0 +1,42 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	slidesPerView: 3,
+	spaceBetween: 30,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		640: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+		},
+		1024: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+	},
+});
